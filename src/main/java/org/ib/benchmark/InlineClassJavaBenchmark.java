7bd3fcbec3cc10d17e7e7ea0bd94e127412bc795
Ionut Balosin <ionutb83@yahoo.com>
2019-02-16 16:02:37 +0100

    Add Inline Class benchmark

---
+++ src/main/java/org/ib/benchmark/InlineClassJavaBenchmark.java
@@ -0,0 +1,11 @@
+package org.ib.benchmark;
+
+/**
+ * @author Ionut Balosin [www.ionutbalosin.com / @ionutbalosin]
+ * @copyright (C) 2019  Ionut Balosin
+ * <p>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+public class InlineClassJavaBenchmark {
+}
