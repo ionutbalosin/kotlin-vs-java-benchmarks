73015df7f1fc43acaba07ebd1da2dd56f6390475
Ionut Balosin <ionutbalosin@yahoo.com>
2019-03-03 11:50:22 +0000

    Update Data Class kotlin

--- src/main/kotlin/org/ib/benchmark/DataClassMain.kt
+++ src/main/kotlin/org/ib/benchmark/DataClassMain.kt
@@ -7,12 +7,12 @@ package org.ib.benchmark;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class Conference(val name: String, val city: String, val year: Int)
+class Conference(val name: String, val city: String?, val year: Int)
 
-data class Talk(val name: String, val rating: Double)
+data class Talk(val name: String, val rating: Double?)
 
 fun main(args: Array<String>) {
-    val conference = Conference("Java", "Fallgate", 2030)
+    val conference = Conference("Java Conference", "Bucharest", 2019)
     println("Welcome to ${conference.name} conference from ${conference.city} city in ${conference.year} year")
 
     //@see https://kotlinlang.org/docs/reference/data-classes.html
@@ -20,4 +20,12 @@ fun main(args: Array<String>) {
     val talk = Talk("Kotlin under the hood", 10.0)
     val (name, rating) = talk
     println("Talk $name has a rating of $rating out of 10")
+
+    // Copy constructors
+    // Below calls copy$default(), the synthetic copy used to create a shallow copy for 0 or different parameters
+    val talkCopy1 =talk.copy()
+    val talkCopy2 =talk.copy(name = "C++")
+    val talkCopy3 =talk.copy(rating = 9.5)
+    // Below calls copy() used to create a shallow copy with all class members
+    val talkCopy4 =talk.copy(name = "C++", rating = 9.5)
 }
